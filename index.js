eee0cc0ac5c2e93a3685c531557ee44fa92d289d
Max Pons <max1ponsve@gmail.com>
2022-09-06 19:14:03 -0500

    Updated UI design Changed the size button: Now, instead of typing the amount of pixels, it gradually increases every time we press it. When you get to the highest amount of pixels possible it returns to to smallest.

--- index.js
+++ index.js
@@ -1,67 +1,99 @@
 const container = document.querySelector(`.container`)
-const btnBlack = document.createElement(`button`)
-const btnEraser = document.createElement(`button`)
-const btnRainbow = document.createElement(`button`)
-const btnSize = document.createElement(`button`)
+const btnBlack = document.querySelector(`#black`)
+const btnEraser = document.querySelector(`#eraser`)
+const btnRainbow = document.querySelector(`#rainbow`)
+const btnSize = document.querySelector(`#size`)
+const btnColor = document.querySelector(`#color`)
 const buttonsContainer = document.querySelector(`.buttons`)
 
-function createDivs(col, rows) {
+let choice = 2;
+let sizeChoice = choice;
+
+function createDivs(col = sizeChoice, rows = sizeChoice) {
 
         for (i = 0; i < (col * rows); i++) {
             const div = document.createElement(`div`)
-            div.style.border = `1px dotted yellow`
-            container.style.gridTemplateColumns = `repeat(${col}, 1fr)`
-            container.style.gridTemplateRows = `repeat(${rows}, 1fr)`
-            container.appendChild(div).classList.add(`box`)
+            div.style.border = `1px white dotted`;
+            container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
+            container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+            container.appendChild(div).classList.add(`box`);
         }
+
+
+}
+function size() {
+    
+
+        if (choice === 2) {
+            
+            return(3);
+           
+        }
+
+        else if (choice === 3) {
+       
+            return(4);
+        }
+        
+        else if (choice === 4) {
+                
+            return(8);
+        }
+        
+        else if (choice === 8) {
+                       
+            return(12);
+        }   
+        else if (choice === 12) {
+                       
+            return(16);
+        }   
+        else if (choice == 16) {
+                       
+            return(2);
+        }   
+        
 }
 
-createDivs(3,3)
+
+
+
+
 
 
 function blackColor() {
     const boxs = document.querySelectorAll(`.box`)
-    btnBlack.textContent = `Black`
-    btnBlack.addEventListener(`click`, () =>{
-        boxs.forEach(box => box.addEventListener(`mouseover`, () =>{
-            /* let Rnum = Math.floor(Math.random() * 255) */
-            let Rnum = 0;
-            box.style.border = `rgb(${Rnum},${Rnum},${Rnum})`
-            box.style.background = `rgb(${Rnum},${Rnum},${Rnum})`
+        btnBlack.addEventListener(`click`, () =>{
+            boxs.forEach(box => box.addEventListener(`mouseover`, () =>{
+                box.style.border = `0`;
+                box.style.background = `black`;
         }))
-    })
+        })
 
-    buttonsContainer.appendChild(btnBlack).classList.add(`btn`)
-}
+    }
 
 function erase() {
     const boxs = document.querySelectorAll(`.box`)
-    btnEraser.textContent = `Eraser`
-    btnEraser.addEventListener(`click`, () =>{
-        boxs.forEach(box => box.addEventListener(`mouseover`, () =>{
-            box.style.border = `1px dotted yellow`
-            box.style.background = `black`
+        btnEraser.addEventListener(`click`, () =>{
+            boxs.forEach(box => box.addEventListener(`mouseover`, () =>{
+                box.style.border = `1px dotted white`
+                box.style.background = `black`
         }))
-    })
-
-    buttonsContainer.appendChild(btnEraser).classList.add(`btn`)
-} 
+        })
+    }    
 
 function rainbowMan() {
     const boxs = document.querySelectorAll(`.box`)
-    btnRainbow.textContent = `Rainbow`
-    btnRainbow.addEventListener(`click`, () =>{
-        boxs.forEach(box => box.addEventListener(`mouseover`, () =>{
-            let R = Math.floor(Math.random() * 255) 
-            let G = Math.floor(Math.random() * 255)
-            let B = Math.floor(Math.random() * 255)
-            box.style.border = `rgb(${R},${G},${B})`
-            box.style.background = `rgb(${R},${G},${B}, ${20})`
+        btnRainbow.addEventListener(`click`, () =>{
+            boxs.forEach(box => box.addEventListener(`mouseover`, () =>{
+                let R = Math.floor(Math.random() * 255) 
+                let G = Math.floor(Math.random() * 255)
+                let B = Math.floor(Math.random() * 255)
+                box.style.border = `rgb(${R},${G},${B})`
+                box.style.background = `rgb(${R},${G},${B}, ${10})`
         }))
-    })
-
-    buttonsContainer.appendChild(btnRainbow).classList.add(`btn`)
-} 
+        })
+    } 
 
 
 
@@ -71,28 +103,26 @@ function reSet() {
 }
 
 function reSize() {
-    btnSize.textContent = `size`;
     btnSize.addEventListener(`click`, () => {
-        let user = prompt(`write the number of squares per side.(e.g. 16)
-Max 64`)
-            if (user < 1 || user === null || user > 64){
-                reSet()
-                createDivs(4, 4)
-                blackColor() 
-                erase()
-                rainbowMan()
-            }
-            else {
-                reSet()
-                createDivs(user, user)
-                blackColor() 
-                erase()
-                rainbowMan()
-            }
-    })
-    buttonsContainer.appendChild(btnSize).classList.add(`btn`)
+
+        reSet()
+        choice = size();
+        sizeChoice = choice;
+        createDivs();
+        blackColor();
+        rainbowMan()
+        erase(); 
+        })
+ 
 }
-reSize()
-blackColor() 
-erase()
+
+reSize();
+createDivs();
+blackColor();
 rainbowMan()
+erase();
+
+
+
+
+
